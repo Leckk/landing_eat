953fcadaeb61cdb797b5798ecd494eb4932e0e2f
Léo Fontaine <leofontaine4@gmail.com>
2019-04-26 14:03:11 +0200

    logo

--- site_vitrine/js/scripts.js
+++ site_vitrine/js/scripts.js
@@ -87,9 +87,6 @@ $(function() {
 
 /***************** Nav Transformicon ******************/
 
-document.querySelector("#nav-toggle").addEventListener("click", function() {
-	this.classList.toggle("active");
-});
 
 /***************** Overlays ******************/
 
